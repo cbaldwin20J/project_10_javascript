20c24c8950f5feb9060b4a9256aed52220dcc261
Chris Baldwin <cbaldwin20@gmail.com>
2018-10-04 09:39:35 -0500

    Did basic routes. Will fix anchor links

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,64 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	console.log("hey");
-	models.Patron.findById(1).then(function(patron){
-    /* render the index page */
-    
-    res.render('index', { title: patron.first_name });
-    /* if there is nothing in the 'Article' table */
-  }).catch(function(error){
-  	res.render('index', { title: error });
-});
+    res.render('home');
 });
   
-  
+router.get('/all_books', function(req, res, next) {
+    res.render('all_books');
+});
+
+router.get('/all_loans', function(req, res, next) {
+    res.render('all_loans');
+});
+
+router.get('/all_patrons', function(req, res, next) {
+    res.render('all_patrons');
+});
+
+router.get('/book_detail', function(req, res, next) {
+    res.render('book_detail');
+});
+
+router.get('/checked_books', function(req, res, next) {
+    res.render('checked_books');
+});
+
+router.get('/check_loans', function(req, res, next) {
+    res.render('check_loans');
+});
+
+router.get('/error', function(req, res, next) {
+    res.render('error');
+});
+
+router.get('/new_book', function(req, res, next) {
+    res.render('new_book');
+});
+
+router.get('/new_loan', function(req, res, next) {
+    res.render('new_loan');
+});
+
+router.get('/new_patron', function(req, res, next) {
+    res.render('new_patron');
+});
+
+router.get('/overdue_books', function(req, res, next) {
+    res.render('overdue_books');
+});
+
+router.get('/overdue_loans', function(req, res, next) {
+    res.render('overdue_loans');
+});
+
+router.get('/patron_detail', function(req, res, next) {
+    res.render('patron_detail');
+});
+
+router.get('/return_book', function(req, res, next) {
+    res.render('return_book');
+});
 
 module.exports = router;
 
