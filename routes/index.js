96eaec63a8c56c94082bd6e26fae659e033417c3
Chris Baldwin <cbaldwin20@gmail.com>
2018-10-04 08:12:11 -0500

    Converted files to pug, will do layout.pug now

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	console.log("hey");
-	models.Book.findById(5).then(function(book){
+	models.Patron.findById(1).then(function(patron){
     /* render the index page */
-    console.log("************here's the books: " + book.title);
-    res.render('index', { title: book.title });
+    
+    res.render('index', { title: patron.first_name });
     /* if there is nothing in the 'Article' table */
   }).catch(function(error){
+  	res.render('index', { title: error });
 });
 });
   
